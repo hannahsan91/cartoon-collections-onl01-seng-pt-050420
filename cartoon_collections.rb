6972679768defb495d5517d2f8bc4d9b89045eb1
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-13 02:23:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,16 +8,16 @@ def summon_captain_planet# code an argument here
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+  short_words = ["puff", "go", "two"]
+  long_planeteer_calls(short_words)
+  assorted_words = ["two", "go", "industrious", "bop"]
+  long_planeteer_calls(assorted_words)
 end
 
 def find_the_cheese(snacks)# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks = ["crackers", "gouda", "thyme"]
-find_the_cheese(snacks)
-#=> "gouda"
-
-soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-find_the_cheese(soup)
+  find_the_cheese(snacks)
+  soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+  find_the_cheese(soup)
 end
